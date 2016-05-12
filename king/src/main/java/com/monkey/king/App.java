14bf7c04de9fbb0167a216a57e94e9538d41bcae
chrislinggit <chris.0times@gmail.com>
2016-05-12 13:18:20 +0800

    test
    test

---
+++ king/src/main/java/com/monkey/king/App.java
@@ -0,0 +1,13 @@
+package com.monkey.king;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Monkey!" );
+    }
+}

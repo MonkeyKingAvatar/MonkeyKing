1e3872a6689c504853130f7673305381a421858f
Tan <mltan@polycom.com>
2016-05-19 11:11:59 +0800

    New Project

---
+++ my-lib/src/test/java/com/polycom/test/PersonTest.java
@@ -0,0 +1,15 @@
+package com.polycom.test;
+
+import org.junit.Test;
+
+import com.polycom.entity.Person;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void canConstructAPersonWithAName() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
+    }
+}
